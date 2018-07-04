85fc4a0ed36a3c55a76246451ed62ad93ca0d0ff
Formotiis <nosvulca@gmail.com>
2018-07-05 01:32:18 +0200

    Edited Battle window, Unit Viewing/Editing gui start

---
+++ src/views/EntityViewer.java
@@ -0,0 +1,29 @@
+package views;
+
+import javax.swing.*;
+
+public class EntityViewer {
+    private JComboBox Class;
+    private JSpinner unitLevel;
+    private JSpinner unitExp;
+    private JLabel unitName;
+    private JSpinner curHP;
+    private JSpinner mxHP;
+    private JSpinner spinner1;
+    private JSpinner spinner2;
+    private JSpinner spinner3;
+    private JSpinner spinner4;
+    private JSpinner spinner5;
+    private JSpinner spinner6;
+    private JCheckBox slot0Eq;
+    private JComboBox comboBox1;
+    private JComboBox comboBox2;
+    private JComboBox comboBox3;
+    private JComboBox comboBox4;
+    private JComboBox comboBox5;
+    private JSpinner spinner7;
+    private JCheckBox slot1Eq;
+    private JCheckBox slot2Eq;
+    private JCheckBox slot3Eq;
+    private JCheckBox slot4Eq;
+}
